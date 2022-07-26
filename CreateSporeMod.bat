5b115578e34f78c5cb5a6f03eb1e72bec28de5b0
Rosalie Wanders <rosalie@mailbox.org>
2022-07-26 15:55:53 +0200

    don't use $(SporeLauncherPath) and link to locally built Spore ModAPI

--- CreateSporeMod.bat
+++ CreateSporeMod.bat
@@ -4,7 +4,7 @@ set CUR_DIR="%~dp0"
 mkdir "%CUR_DIR%\obj"
 mkdir "%CUR_DIR%\bin"
 
-copy "C:\ProgramData\SPORE ModAPI Launcher Kit\mLibs\SporeAutoSave.dll" "%CUR_DIR%\obj\"
+copy "%CUR_DIR%\SporeAutoSave\Bin\Release\SporeAutoSave.dll" "%CUR_DIR%\obj\"
 copy "%CUR_DIR%\ModInfo.xml" "%CUR_DIR%\obj\"
 
 cd "%CUR_DIR%\obj\"
